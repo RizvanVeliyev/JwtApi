adc8bf4010981b467f32f2581668854e0a14ae93
Rizvan Valiyev <rvaliyev@rth.gov.az>
2025-07-30 08:57:48 +0400

    2 factor done!

--- Application/Features/Auth/Commands/Register/RegisterResponseDto.cs
+++ Application/Features/Auth/Commands/Register/RegisterResponseDto.cs
@@ -5,6 +5,7 @@
         public string Id { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
+        public bool TwoFactorEnabled { get; set; }
     }
 
 }
